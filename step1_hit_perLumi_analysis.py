b34c5a5468fdf22cc63a4c560071322c09edad2e
Francesco Ivone <francesco.ivone@cern.ch>
2022-04-07 17:07:58 +0200

    evt number is now an unsigned long

--- step1_hit_perLumi_analysis.py
+++ step1_hit_perLumi_analysis.py
@@ -81,7 +81,7 @@ if __name__ == "__main__":
     outTree1.Branch("nhit_P2", nhit_P2,"nhit_P2[36][24]/I")
     
     ##branches in output file
-    evt_2 = array('I', [0])
+    evt_2 = array('L', [0])
     run_2  = array('i', [0])
     lumi_2 = array('i', [0])
     nhit_M1_2 = np.zeros(shape=(36), dtype=np.uint32)
